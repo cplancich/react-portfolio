35a0f15277717cb1ecee737343a71b4263664bf2
colinplancich <colin.plancich@gmail.com>
2022-10-24 11:36:53 -0700

    Started Navigation component & linked to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-sky-500">
-      Hello world!
-    </h1>
+    <>
+      <Navigation />
+    </>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+export const Navigation = () => {
+    return (
+    <div className="bg-sky-500 text-white flex">
+        <nav className="ml-auto">
+            <a href="#Profile" className="p-2 hover:bg-sky-700">Profile</a>
+            <a href="#Portfolio" className="p-2 hover:bg-sky-700">Portfolio</a>
+            <a href="#Contact" className="p-2 hover:bg-sky-700">Contact</a>
+        </nav>
+    </div>
+    )
+}
